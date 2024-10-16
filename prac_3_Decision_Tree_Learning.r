a469fc79dbeae6563a6788e79591b27b23ee4877
Runch <105694433+runch02@users.noreply.github.com>
2024-10-17 00:39:54 +0530

    Update prac_3_Decision_Tree_Learning.r

--- prac_3_Decision_Tree_Learning.r
+++ prac_3_Decision_Tree_Learning.r
@@ -1,27 +1,11 @@
 install.packages("rpart")
 library(rpart)
-
 install.packages("rpart.plot")
 library(rpart.plot)
-
-# Get the current working directory
 getwd()
-
-# Set the working directory
 setwd("D:/AIPRAC")
-
-# Read the dataset
 d <- read.csv("restaurant.csv")
-
-# Create the decision tree model
 mytree <- rpart(Wait ~ ., data = d, minsplit = 1, minbucket = 1)
-
-# Plot the decision tree
 rpart.plot(mytree)
-
-# Make predictions
 p <- predict(mytree)
 p
-
-
-prac_3
