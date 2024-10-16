788aa101e14881bddce49227f63059349b1807e4
Runch <105694433+runch02@users.noreply.github.com>
2024-10-17 00:42:07 +0530

    Update prac_8_Association_Rule_Mining.py

--- prac_8_Association_Rule_Mining.py
+++ prac_8_Association_Rule_Mining.py
@@ -1,11 +1,12 @@
 from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+import numpy as np
+
 X, Y = make_blobs(n_samples=500, centers=2, random_state=0, cluster_std=0.40)
 
-import matplotlib.pyplot as plt
 plt.scatter(X[:, 0], X[:, 1], c=Y, s=50, cmap='spring')
 plt.show()
 
-import numpy as np
 xfit = np.linspace(-1, 3.5)
 
 plt.scatter(X[:, 0], X[:, 1], c=Y, s=50, cmap='spring')
@@ -18,6 +19,3 @@ plt.xlim(-1, 3.5)
 plt.show()
 
 print(X, Y)
-
-
-prac_8
